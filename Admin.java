685918574ff035fa527cd2bffd551491cc5d1b6c
1602-20-737-020-leena <91466801+1602-20-737-020-leena@users.noreply.github.com>
2022-07-01 23:16:05 +0530

    Add files via upload

---
+++ Admin.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+
+/** 
+ * @author Administrator
+ * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+ */
+public class Admin extends Allotttee {
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String username;
+
+	/**
+	 * @return the username
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public String getUsername() {
+		// begin-user-code
+		return username;
+		// end-user-code
+	}
+
+	/**
+	 * @param theUsername the username to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setUsername(String theUsername) {
+		// begin-user-code
+		username = theUsername;
+		// end-user-code
+	}
+
+	/** 
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	private String password;
+
+	/**
+	 * @return the password
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public String getPassword() {
+		// begin-user-code
+		return password;
+		// end-user-code
+	}
+
+	/**
+	 * @param thePassword the password to set
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void setPassword(String thePassword) {
+		// begin-user-code
+		password = thePassword;
+		// end-user-code
+	}
+
+	/**
+	 * @param username
+	 * @param password
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void login(Object username, Object password) {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/**
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void viewfeedetails() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/**
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void searchanallotte() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+
+	/**
+	 * @generated "UML to Java V5.0 (com.ibm.xtools.transform.uml2.java5.internal.UML2JavaTransform)"
+	 */
+	public void viewvisitordetails() {
+		// begin-user-code
+		// TODO Auto-generated method stub
+
+		// end-user-code
+	}
+}
